e2ed6936675e1a2968a86613a768c22fe4929f95
Nikhil kumawat <112697521+BTWITSNIKHIL@users.noreply.github.com>
2024-07-16 17:17:53 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/BTWITSNIKHIL",
   resume:
-    "https://drive.google.com/file/d/1esNNpiOTzNSLcsyWbf7W-syXydSjR4Vk/view?usp=sharing",
+    "https://drive.google.com/file/d/1aaUTa3dlocuxxpg1GlsIaHZqzNSlbJiM/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/nikhil-kumawat-21316b256",
   twitter: "https://x.com/NikhilKuma4829",
 };
